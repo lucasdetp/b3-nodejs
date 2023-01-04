2184cefd7bac361b210dd3d443cb673307730675
lucasdetp <detplucas@mail.com>
2023-01-04 11:55:14 +0100

    first commit

---
+++ src/routes/getRegister.ts
@@ -0,0 +1,9 @@
+import { Application } from "express-ws";
+import path from "path";
+
+export function getRegister(app: Application) {
+    app.get('/register', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../pages/register.html'))
+    })
+
+}
---
+++ src/routes/postRegister.ts
@@ -0,0 +1,23 @@
+import bodyParser from "body-parser";
+import { Application } from "express-ws";
+import { createUser, findUserByEmail } from "../repositories/userRepository";
+
+export function postRegister(app: Application) {
+    app.post('/register', bodyParser.urlencoded(),
+        async (req, res) => {
+            const { email, name } = req.body;
+            if (!email || name) {
+                res.status(400).send('Bad Request')
+                return
+            }
+            const user = await createUser(email, name);
+            if (!user) {
+                res.status(400).send('Email already used')
+                return
+            }
+            res.cookie('ssid', user.id, {
+                signed: true, httpOnly: true, sameSite: true
+            });
+            res.redirect('/');
+        })
+}
